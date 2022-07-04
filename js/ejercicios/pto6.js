25a63705fd6953f9afc6fdbb67dcf56078014f79
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-04 19:39:51 -0300

    realice el punto 6 de condicionales

---
+++ js/ejercicios/pto6.js
@@ -0,0 +1,7 @@
+let numero1 = parseInt(prompt("Ingrese un primer numero"))
+let numero2 = parseInt(prompt("Ingrese un segundo numero"))
+if (numero1 > numero2){
+    document.write ("El primer numero es mayor, el numero ingresado es: "+ numero1)
+} else if (numero1 < numero2) {
+    document.write ("El segundo numero es mayor, el numero ingresado es: "+ numero2)
+}
--- js/variables.js
+++ js/variables.js
@@ -6,6 +6,3 @@ nombreUsuario = (prompt("Hola nombreUsuario"))
 let numero1 = parseInt(prompt ("Ingrese un primer numero"))
 let numero2 = parseInt(prompt ("Ingrese un segundo numero"))
 document.write ("El resultado es "+ (numero1 + numero2))
-
-
-
