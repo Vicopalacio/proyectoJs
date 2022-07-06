114d171db114a40ed3ef1c9fa84f516de5b29827
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-06 16:22:26 -0300

    corregi punto 8

--- js/ejercicios/pto8.js
+++ js/ejercicios/pto8.js
@@ -2,6 +2,6 @@ let numero1 = parseInt (prompt ("ingrese un numero"))
 
 console.log (numero1)
 
-if (numero1 /2){
-    document.write ("Este numero si es divisible: " + (numero1 /2))
+if (numero1 %2){
+    document.write ("Este numero si es divisible: " + (numero1 %2))
 }
