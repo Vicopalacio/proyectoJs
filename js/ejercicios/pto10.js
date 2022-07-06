13f52b8428e29c34dca30e8c2c4e6b68f841fcba
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-06 17:13:06 -0300

    modifique punto 10

--- js/ejercicios/pto10.js
+++ js/ejercicios/pto10.js
@@ -2,14 +2,14 @@ let numero1 = parseInt (prompt ("ingrese un numero"))
 
 console.log(numero1)
 
-if (numero1 /2){
-    document.write("Este numero es divisible por 2: " + (numero1 /2))
-}else if (numero1 /3){
-    document.write("Este numero es divisible por 3: " + (numero1 /3))
-}else if (numero1 /5){
-    document.write("Este numero es divisible por 5: " + (numero1 /5))
-}else if (numero1 /7){
-    document.write("Este numero es divisible por 7: " + (numero1 /7))
+if (numero1 %2){
+    document.write("Este numero es divisible por 2: " + (numero1 %2))
+}else if (numero1 %3){
+    document.write("Este numero es divisible por 3: " + (numero1 %3))
+}else if (numero1 %5){
+    document.write("Este numero es divisible por 5: " + (numero1 %5))
+}else if (numero1 %7){
+    document.write("Este numero es divisible por 7: " + (numero1 %7))
 }else {
     document.write("Este numero no es divisible")
 }
