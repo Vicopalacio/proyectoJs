6b68ca54951a016d4141ec9e5b944b6e3aab7ad5
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-06 18:04:27 -0300

    realice finalmente el punto 11

--- js/ejercicios/punto11.js
+++ js/ejercicios/punto11.js
@@ -4,12 +4,15 @@ console.log (numero1)
 
 if (numero1 %2){
     document.write("Este numero es divisible por 2: " + (numero1 %2))
-}else if (numero1 %3){
-    document.write("Este numero es divisible por 3: " + (numero1 %3))
-}else if (numero1 %5){
-    document.write("Este numero es divisible por 5: " + (numero1 %5))
-}else if (numero1 %7){
-    document.write("Este numero es divisible por 7: " + (numero1 %7))
+}
+if (numero1 %3){
+    document.write(" Este numero es divisible por 3: " + (numero1 %3))
+}
+if (numero1 %5){
+    document.write(" Este numero es divisible por 5: " + (numero1 %5))
+}
+if (numero1 %7){
+    document.write(" Este numero es divisible por 7: " + (numero1 %7))
 }else {
-    document.write("Este numero no es divisible")
+    document.write(" Este numero no es divisible")
 }
